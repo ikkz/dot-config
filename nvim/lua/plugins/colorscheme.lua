34cbc3f3b055641e1a3855b282c0824129046d96
ikkz <ylei.mk@gmail.com>
2026-01-03 14:46:56 +0800

    update

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,17 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "sainnhe/everforest",
+    lazy = false,
     priority = 1000,
+    config = function()
+      vim.g.everforest_background = "soft"
+      vim.g.everforest_better_performance = 1
+    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-latte",
+      colorscheme = "everforest",
     },
   },
 }
